28d06df4ad0af43f2780b1c7ea0b4da71a3a1579
Khalil Virji <46275645+khalilv@users.noreply.github.com>
2020-03-09 22:38:08 -0400

    added distributeItems after fight

---
+++ src/main/java/LoAServer/FightDistribution.java
@@ -0,0 +1,88 @@
+package LoAServer;
+
+public class FightDistribution {
+    private int warriorGold;
+    private int archerGold;
+    private int wizardGold;
+    private int dwarfGold;
+    private int warriorWillpower;
+    private int archerWillpower;
+    private int wizardWillpower;
+    private int dwarfWillpower;
+
+    public FightDistribution() {
+        warriorGold = 0;
+        archerGold = 0;
+        wizardGold = 0;
+        dwarfGold = 0;
+        warriorWillpower = 0;
+        archerWillpower = 0;
+        wizardWillpower = 0;
+        dwarfWillpower = 0;
+    }
+
+
+    public int getWarriorGold() {
+        return warriorGold;
+    }
+
+    public void setWarriorGold(int warriorGold) {
+        this.warriorGold = warriorGold;
+    }
+
+    public int getArcherGold() {
+        return archerGold;
+    }
+
+    public void setArcherGold(int archerGold) {
+        this.archerGold = archerGold;
+    }
+
+    public int getWizardGold() {
+        return wizardGold;
+    }
+
+    public void setWizardGold(int wizardGold) {
+        this.wizardGold = wizardGold;
+    }
+
+    public int getDwarfGold() {
+        return dwarfGold;
+    }
+
+    public void setDwarfGold(int dwarfGold) {
+        this.dwarfGold = dwarfGold;
+    }
+
+    public int getWarriorWillpower() {
+        return warriorWillpower;
+    }
+
+    public void setWarriorWillpower(int warriorWillpower) {
+        this.warriorWillpower = warriorWillpower;
+    }
+
+    public int getArcherWillpower() {
+        return archerWillpower;
+    }
+
+    public void setArcherWillpower(int archerWillpower) {
+        this.archerWillpower = archerWillpower;
+    }
+
+    public int getWizardWillpower() {
+        return wizardWillpower;
+    }
+
+    public void setWizardWillpower(int wizardWillpower) {
+        this.wizardWillpower = wizardWillpower;
+    }
+
+    public int getDwarfWillpower() {
+        return dwarfWillpower;
+    }
+
+    public void setDwarfWillpower(int dwarfWillpower) {
+        this.dwarfWillpower = dwarfWillpower;
+    }
+}
